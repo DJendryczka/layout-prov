213f2db41081b6fc489ec6d148432560e5c8256e
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-04-12 20:27:06 +0200

    Added list and contact section

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const hamburgerLinks = document.getElementById('hamburger-links');
 hamburgerButton.addEventListener('click', ()=>{
     hamburgerLinks.classList.toggle('navbar-open')
 })
+
+/* Modal */
+
+const openBtn = document.getElementById('open-modal');
+const closeBtn = document.getElementById('close-modal');
+
+openBtn.addEventListener('click', ()=>{
+    document.getElementById('overlay').style.display='block'
+})
+
+closeBtn.addEventListener('click', ()=>{
+    document.getElementById('overlay').style.display='none'
+})
